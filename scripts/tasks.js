51800e663aafff3c79d49f92fba54fe259e7fe0f
arsene_001 <djumbearsene@gmail.com>
2022-05-13 01:49:22 +0300

    Updating the JS form validation

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -3,39 +3,38 @@ const text_el = document.querySelector("#text")
 const text_el_value = document.querySelector("#text").value;
 const tasks_el = document.querySelector("#tasks_el")
 window.addEventListener('load', () => {
-
+ 
   submit_el.addEventListener("click", (e) => {
-    
-    
-    if (text_el.value.length > 0) {
-      render()
-      return false;
-    }
-    else {
-     return;
-    }
-  function render() {
+    e.preventDefault();
+
       const tasks_container = document.createElement("div");
         tasks_container.classList.add("tasks_container");
-
-        tasks_el.appendChild(tasks_container)
 
       const contents = document.createElement("div");
         contents.classList.add("contents");
 
-        tasks_container.appendChild(contents);
+        
 
       const input_el = document.createElement("textarea");
         input_el.classList.add("input_el");
         input_el.setAttribute("readonly", "readonly");
-        input_el.innerHTML = text_el.value;
 
+        if (text_el.value) {
+          input_el.innerHTML = text_el.value;
+        }
+
+        else {
+          alert("Please fill out the form!")
+          return false;
+        }
         contents.appendChild(input_el);
+
+        tasks_container.appendChild(contents);
 
       const actions = document.createElement("div");
         actions.classList.add("actions");
 
-        tasks_container.appendChild(actions);
+        
 
       const edit_el = document.createElement("button");
         edit_el.classList.add("edit_el");
@@ -48,15 +47,16 @@ window.addEventListener('load', () => {
         actions.appendChild(edit_el);
         actions.appendChild(delete_el);
 
+        tasks_container.appendChild(actions);
+
+        tasks_el.appendChild(tasks_container)
+
         text_el.value = "";
+        render();
+    
 
-   
-
-  document.querySelector(".delete_el").addEventListener('click', () => {
-    tasks_el.removeChild(tasks_container);
-  })
-
-  document.querySelector(".edit_el").addEventListener('click', () => {
+  function render() {  
+  edit_el.addEventListener('click', () => {
     if (edit_el.innerHTML.toLowerCase() == "edit") {
       input_el.removeAttribute("readonly");
       input_el.focus();
@@ -69,6 +69,13 @@ window.addEventListener('load', () => {
     }
   })
 
-}
+  delete_el.addEventListener('click', () => {
+    tasks_el.removeChild(tasks_container);
   })
+
+}
+ 
+})
+
+  
 })
